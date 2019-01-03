d7b000d8da407a7a36af3d50343dc43577db9116
zhengzhirong <zhengzwing@gmail.com>
2019-01-03 20:08:01 +0800

    test: 💍 add test case

--- test/test.js
+++ test/test.js
@@ -6,6 +6,24 @@ const generator = path.join(__dirname, '..')
 
 test('defaults', async t => {
   const stream = await sao.mock({ generator })
-
   t.snapshot(stream.fileList, 'Generated files')
+})
+
+test('test sass', async t => {
+  const stream = await sao.mock({ generator }, {
+    useSass: true
+  })
+  const { readFile, fileList, api } = stream
+  t.true(fileList.filter(each => each === 'postcss.config.js').length === 1)
+  // console.log(fileList);
+  let pkg = await readFile('package.json')
+  pkg = JSON.parse(pkg)
+  const {devDependencies} = pkg
+  t.true('node-sass' in devDependencies)
+  t.true('rollup-plugin-postcss' in devDependencies)
+  t.true('docz-plugin-css' in devDependencies)
+  t.true('autoprefixer' in devDependencies)
+  const doczrc = await readFile('doczrc.js')
+  t.snapshot(doczrc, 'doczrc with sass')
+  // t.true()
 })
